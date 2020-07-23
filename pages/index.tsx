76c5b31d7a6db3f88cbdc65b73a482382be9482e
takeshi.kano <t.kano.624@gmail.com>
2020-07-23 23:43:36 +0900

    トップページ

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,47 @@
 import Head from "next/head";
 import React from "react";
 import { GetStaticProps } from "next";
-import axios from "axios";
+import { BlogType } from "../types/BlogType";
+import styles from "./index.module.scss";
+import { SideNavigation } from "../components/SideNavigation";
+import { EntryList } from "../components/EntryList";
 
-type BlogType = {
-  id: string;
-  name: string;
-};
+const contentful = require("contentful");
+
+const client = contentful.createClient({
+  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+});
 
 export const getStaticProps: GetStaticProps = async () => {
-  const key = {
-    headers: { "X-API-KEY": process.env.API_KEY },
-  };
+  const entries: {
+    items: {
+      sys: {
+        id: string;
+      };
+      fields: {
+        title: string;
+        published_date: string;
+        url: string;
+        medium: {
+          fields: {
+            title: string;
+            slug: string;
+          };
+        };
+      };
+    }[];
+  } = await client.getEntries({
+    content_type: "portfolio",
+  });
 
-  const response = await axios.get(
-    "https://kano.microcms.io/api/v1/portfolio",
-    key
-  );
-  const blogs = (await response.data.contents) as BlogType[];
+  const blogs: BlogType[] = entries.items.map((entry) => {
+    return {
+      ...entry.fields,
+      id: entry.sys.id,
+      medium: entry.fields.medium.fields,
+    };
+  });
 
   return {
     props: {
@@ -34,11 +58,17 @@ const Index: React.FC<{
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap"
+          rel="stylesheet"
+        />
       </Head>
-
-      {blogs.map((blog) => (
-        <li key={blog.id}>{blog.name}</li>
-      ))}
+      <div className={styles.wrapper}>
+        <SideNavigation />
+        <main className={styles.main}>
+          <EntryList blogData={blogs} />
+        </main>
+      </div>
     </div>
   );
 };
