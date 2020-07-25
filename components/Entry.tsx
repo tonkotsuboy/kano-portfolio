3e566defb0bade0ace7b4e2740233a01621f9e7c
takeshi.kano <t.kano.624@gmail.com>
2020-07-25 19:33:59 +0900

    ✨エントリー：詳細ページに飛べるように

--- components/Entry.tsx
+++ components/Entry.tsx
@@ -11,30 +11,26 @@ type Props = {
  * 各記事のエントリー
  */
 export const Entry: React.FC<Props> = ({
-  entryData: { id, published_date, title, url, medium, tags },
+  entryData: { id, published_date, title, medium, tags },
 }) => (
-  <div className={styles.entry}>
-    <header className={styles.header}>
-      <a className={styles.medium} href={medium.slug}>
-        {medium.name}
-      </a>
-      <ul className={styles.taglist}>
-        {tags.map(({ name, slug }) => (
-          <li key={slug} className={styles.tag}>
-            <a href={`/tags/${slug}`}>#{name}</a>
-          </li>
-        ))}
-      </ul>
-    </header>
-    <h2 className={styles.title}>{title}</h2>
-    <Link href={`/entry/${id}`}>
-      <a>{id}</a>
-    </Link>
-    <a className={styles.link} href={url}>
-      {url}
+  <Link href={`/entry/${id}`}>
+    <a className={styles.entry}>
+      <header className={styles.header}>
+        <a className={styles.medium} href={medium.slug}>
+          {medium.name}
+        </a>
+        <ul className={styles.taglist}>
+          {tags.map(({ name, slug }) => (
+            <li key={slug} className={styles.tag}>
+              <a href={`/tags/${slug}`}>#{name}</a>
+            </li>
+          ))}
+        </ul>
+      </header>
+      <h2 className={styles.title}>{title}</h2>
+      <time dateTime={published_date} className={styles.published_date}>
+        {published_date}
+      </time>
     </a>
-    <time dateTime={published_date} className={styles.published_date}>
-      {published_date}
-    </time>
-  </div>
+  </Link>
 );
