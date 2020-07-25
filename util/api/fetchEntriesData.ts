4b3e99c37778caaf6d22c3cb22a9daae746a5f1e
takeshi.kano <t.kano.624@gmail.com>
2020-07-25 18:59:22 +0900

    ✨API：データ通信用メソッド

---
+++ util/api/fetchEntriesData.ts
@@ -0,0 +1,31 @@
+import { ContentfulClientApi, Entry, EntryCollection } from "contentful";
+
+const contentful = require("contentful");
+
+/**
+ * contentful用のclientAPIです
+ */
+const client: ContentfulClientApi = contentful.createClient({
+  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+});
+
+/**
+ * content_typeを指定して、エントリーデータをフェッチします
+ * @param contentType
+ */
+export const fetchEntriesData = async <T>(
+  contentType: string
+): Promise<EntryCollection<T>> => {
+  return await client.getEntries<T>({
+    content_type: contentType,
+  });
+};
+
+/**
+ * entry_idを指定して、エントリーデータを取得します
+ * @param entryId
+ */
+export const fetchEntryData = async <T>(entryId: string): Promise<Entry<T>> => {
+  return await client.getEntry(entryId);
+};
