e064b105059dd12e5b05b25f26dc3af940ac0b96
takeshi.kano <t.kano.624@gmail.com>
2020-07-25 19:33:26 +0900

    ✨エントリーページ：タグの表示

--- pages/entry/[id].tsx
+++ pages/entry/[id].tsx
@@ -6,6 +6,10 @@ import {
 } from "../../util/api/fetchEntriesData";
 
 import React from "react";
+import { IndexContext, IndexContextType } from "../../contexts/IndexContext";
+import BasePage from "../../components/base/BasePage";
+import { TagType } from "../../types/client/TagType";
+import { TagModel } from "../../types/server/TagModel";
 
 export const getStaticPaths: GetStaticPaths = async () => {
   // Get the paths we want to pre-render based on posts
@@ -24,19 +28,38 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
       },
     };
   }
-  const entryData: PortfolioModel = (
-    await fetchEntryData<PortfolioModel>(entryId)
-  ).fields;
+  const [entryData, tagData] = await Promise.all([
+    fetchEntryData<PortfolioModel>(entryId),
+    fetchEntriesData<TagModel>("tag"),
+  ]);
+
+  const tagList: TagType[] = tagData.items.map((item) => {
+    return item.fields;
+  });
 
   return {
     props: {
-      entryData,
+      tagList,
+      entryData: entryData.fields,
     },
   };
 };
 
-const EntryPage: React.FC<{ entryData: PortfolioModel }> = ({ entryData }) => {
-  return <p>{entryData.title}やで</p>;
+const EntryPage: React.FC<{
+  tagList: TagType[];
+  entryData: PortfolioModel;
+}> = ({ tagList, entryData }) => {
+  const contextValue: IndexContextType = {
+    tagList,
+  };
+
+  return (
+    <IndexContext.Provider value={contextValue}>
+      <BasePage>
+        <p>{entryData.title}やで</p>
+      </BasePage>
+    </IndexContext.Provider>
+  );
 };
 
 export default EntryPage;
