f2ea5f70a04f5a1c6ad4d30aee615830c0cbee49
takeshi.kano <t.kano.624@gmail.com>
2020-07-25 19:30:38 +0900

    ✨TOP：共通ページの作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,19 @@
-import Head from "next/head";
 import React from "react";
 import { GetStaticProps } from "next";
 import { BlogType } from "../types/client/BlogType";
-import styles from "./index.module.scss";
-import { SideNavigation } from "../components/SideNavigation";
 import { EntryList } from "../components/EntryList";
 import { IndexContext, IndexContextType } from "../contexts/IndexContext";
 import { TagType } from "../types/client/TagType";
 import { PortfolioModel } from "../types/server/PortfolioModel";
 import { TagModel } from "../types/server/TagModel";
 import { fetchEntriesData } from "../util/api/fetchEntriesData";
+import BasePage from "../components/base/BasePage";
 
 export const getStaticProps: GetStaticProps = async () => {
   const [portfolioData, tagData] = await Promise.all([
     fetchEntriesData<PortfolioModel>("portfolio"),
     fetchEntriesData<TagModel>("tag"),
   ]);
-
-  portfolioData.items;
 
   const blogList: BlogType[] = portfolioData.items.map((entry) => {
     const tags: BlogType["tags"] = entry.fields.tags.map(
@@ -54,24 +50,11 @@ const Index: React.FC<{
   };
 
   return (
-    <div className="container">
-      <Head>
-        <title>鹿野ポートフォリオ</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <IndexContext.Provider value={contextValue}>
-        <div className={styles.wrapper}>
-          <SideNavigation />
-          <main className={styles.main}>
-            <EntryList />
-          </main>
-        </div>
-      </IndexContext.Provider>
-    </div>
+    <IndexContext.Provider value={contextValue}>
+      <BasePage>
+        <EntryList />
+      </BasePage>
+    </IndexContext.Provider>
   );
 };
 
