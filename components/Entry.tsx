28bb430d67b627940cf21d5a760b45f68463507e
takeshi.kano <t.kano.624@gmail.com>
2020-07-25 19:04:59 +0900

    ✨TOP：タグの表示

--- components/Entry.tsx
+++ components/Entry.tsx
@@ -11,32 +11,30 @@ type Props = {
  * 各記事のエントリー
  */
 export const Entry: React.FC<Props> = ({
-         entryData: { id, published_date, title, url, medium, tags },
-       }) => {
-         return (
-           <div className={styles.entry}>
-             <header className={styles.header}>
-               <a className={styles.medium} href={medium.slug}>
-                 {medium.name}
-               </a>
-               <ul className={styles.taglist}>
-                 {tags.map(({ name, slug }) => (
-                   <li key={slug} className={styles.tag}>
-                     <a href={`/tags/${slug}`}>#{name}</a>
-                   </li>
-                 ))}
-               </ul>
-             </header>
-             <h2 className={styles.title}>{title}</h2>
-             <Link href={`/entry/${id}`}>
-               <a>{id}</a>
-             </Link>
-             <a className={styles.link} href={url}>
-               {url}
-             </a>
-             <time dateTime={published_date} className={styles.published_date}>
-               {published_date}
-             </time>
-           </div>
-         );
-       };
+  entryData: { id, published_date, title, url, medium, tags },
+}) => (
+  <div className={styles.entry}>
+    <header className={styles.header}>
+      <a className={styles.medium} href={medium.slug}>
+        {medium.name}
+      </a>
+      <ul className={styles.taglist}>
+        {tags.map(({ name, slug }) => (
+          <li key={slug} className={styles.tag}>
+            <a href={`/tags/${slug}`}>#{name}</a>
+          </li>
+        ))}
+      </ul>
+    </header>
+    <h2 className={styles.title}>{title}</h2>
+    <Link href={`/entry/${id}`}>
+      <a>{id}</a>
+    </Link>
+    <a className={styles.link} href={url}>
+      {url}
+    </a>
+    <time dateTime={published_date} className={styles.published_date}>
+      {published_date}
+    </time>
+  </div>
+);
