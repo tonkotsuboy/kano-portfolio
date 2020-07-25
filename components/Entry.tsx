e4415b9e47eec5149588c03c501274b8fcf8ca51
takeshi.kano <t.kano.624@gmail.com>
2020-07-25 19:02:35 +0900

    ✨API：データ供給をContextに

--- components/Entry.tsx
+++ components/Entry.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
-import { BlogType } from "../types/BlogType";
+import { BlogType } from "../types/client/BlogType";
 import styles from "./Entry.module.scss";
+import Link from "next/link";
 
 type Props = {
   entryData: BlogType;
@@ -10,31 +11,32 @@ type Props = {
  * 各記事のエントリー
  */
 export const Entry: React.FC<Props> = ({
-  entryData: { published_date, medium, title, url, detail },
-}) => {
-  console.dir(detail);
-  return (
-    <div className={styles.entry}>
-      <header className={styles.header}>
-        <a className={styles.medium} href={medium.slug}>
-          {medium.title}
-        </a>
-      </header>
-      <h2 className={styles.title}>{title}</h2>
-      <a className={styles.link} href={url}>
-        {url}
-      </a>
-      {/*<p>{detail}</p>*/}
-      {/*<ul>*/}
-      {/*  {tags.map(({ name, slug }) => (*/}
-      {/*    <li>*/}
-      {/*      <a href={`/tags/${slug}`}> {name}</a>*/}
-      {/*    </li>*/}
-      {/*  ))}*/}
-      {/*</ul>*/}
-      <time dateTime={published_date} className={styles.published_date}>
-        {published_date}
-      </time>
-    </div>
-  );
-};
+         entryData: { id, published_date, title, url, medium, tags },
+       }) => {
+         return (
+           <div className={styles.entry}>
+             <header className={styles.header}>
+               <a className={styles.medium} href={medium.slug}>
+                 {medium.name}
+               </a>
+               <ul className={styles.taglist}>
+                 {tags.map(({ name, slug }) => (
+                   <li key={slug} className={styles.tag}>
+                     <a href={`/tags/${slug}`}>#{name}</a>
+                   </li>
+                 ))}
+               </ul>
+             </header>
+             <h2 className={styles.title}>{title}</h2>
+             <Link href={`/entry/${id}`}>
+               <a>{id}</a>
+             </Link>
+             <a className={styles.link} href={url}>
+               {url}
+             </a>
+             <time dateTime={published_date} className={styles.published_date}>
+               {published_date}
+             </time>
+           </div>
+         );
+       };
