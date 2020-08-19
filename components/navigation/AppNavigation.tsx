d9d3ddbf73f0e1413b2d3169cfcf327f9d2f4097
takeshi.kano <t.kano.624@gmail.com>
2020-08-19 22:24:48 +0900

    ✨ナビゲーション：ハンバーガー更新

--- components/navigation/AppNavigation.tsx
+++ components/navigation/AppNavigation.tsx
@@ -21,26 +21,43 @@ export const AppNavigation: React.FC<Props> = ({ className }) => {
         .filter((value) => value != null)
         .join(" ")}
     >
-      <svg
+      <button
+        type="button"
         className={styles.menubutton}
-        width="40px"
-        height="40px"
-        viewBox="0 0 500 500"
         onClick={() => setIsOpen(!isOpen)}
       >
-        <path
-          className={`border1 ${styles.menuborder}`}
-          d="M44.6,151.6h409.9c18.9,0,34.2-15.2,34.2-34.1c0-18.8-15.3-34-34.2-34H44.6c-18.8,0-34.1,15.2-34.1,34 C10.4,136.4,25.7,151.6,44.6,151.6z"
-        />
-        <path
-          className={`border2 ${styles.menuborder}`}
-          d="M454.5,219.9H44.5c-18.8,0-34.1,15.3-34.1,34.1c0,18.9,15.3,34.2,34.1,34.2h409.9c18.9,0,34.2-15.3,34.2-34.2 C488.7,235.2,473.3,219.9,454.5,219.9z"
-        />
-        <path
-          className={`border3 ${styles.menuborder}`}
-          d="M454.5,356.7H44.5c-18.8,0-34.1,15.2-34.1,34.1c0,18.8,15.3,34.1,34.1,34.1h409.9c18.9,0,34.2-15.3,34.2-34.1 S473.3,356.7,454.5,356.7z"
-        />
-      </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="12"
+          viewBox="0 0 20 12"
+        >
+          <g transform="translate(-108.5 -160.5)">
+            <line
+              x2="20"
+              transform="translate(108.5 161.5)"
+              fill="none"
+              stroke="#3f3f9d"
+              strokeWidth="2"
+            />
+            <line
+              x2="20"
+              transform="translate(108.5 166.5)"
+              fill="none"
+              stroke="#3f3f9d"
+              strokeWidth="2"
+            />
+            <line
+              x2="20"
+              transform="translate(108.5 171.5)"
+              fill="none"
+              stroke="#3f3f9d"
+              strokeWidth="2"
+            />
+          </g>
+        </svg>
+      </button>
+
       <h1 className={styles.author}>
         <Link href="/">
           <a>Takeshi Kano</a>
