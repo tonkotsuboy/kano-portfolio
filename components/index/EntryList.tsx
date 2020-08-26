90fb095c176dbd160787d9706a5fda586be40b7e
takeshi.kano <t.kano.624@gmail.com>
2020-08-26 23:58:27 +0900

    ✨記事一覧：リンクにkey指定

--- components/index/EntryList.tsx
+++ components/index/EntryList.tsx
@@ -16,7 +16,7 @@ export const EntryList: React.FC = () => {
       {entryDataList.map((entryData) => {
         if (entryData.medium.slug === "writing") {
           return (
-            <a href={entryData.url} rel="noopener noreferrer" target="_blank">
+            <a key={entryData.id} href={entryData.url} rel="noopener noreferrer" target="_blank">
               <EntryArticle entryData={entryData} isLinkEntry />
             </a>
           );
