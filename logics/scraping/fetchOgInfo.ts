95f4d17aa15fdab534e4269e65d9da17f640847f
takeshi.kano <t.kano.624@gmail.com>
2020-08-29 14:36:44 +0900

    🐞記事詳細：ogimageが表示されないバグ修正

--- logics/scraping/fetchOgInfo.ts
+++ logics/scraping/fetchOgInfo.ts
@@ -4,9 +4,9 @@ export const fetchOgInfo = async (
   const urlText = await (await fetch(url)).text();
 
   const ogImage =
-    /property="og:image" content="(.*?)"\/>/.exec(urlText)?.[1] ?? null;
+    /property="og:image" content="(.*?)"/.exec(urlText)?.[1] ?? null;
   const ogTitle =
-    /property="og:title" content="(.*?)".*>/.exec(urlText)?.[1] ?? null;
+    /property="og:title" content="(.*?)"/.exec(urlText)?.[1] ?? null;
 
   return { ogImage, ogTitle };
 };
