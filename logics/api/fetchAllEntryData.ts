f9e7dcbc260f5110399d0b7dcd691f12ca29ba0b
takeshi.kano <t.kano.624@gmail.com>
2020-08-29 17:53:29 +0900

    ✨全データフェッチ：発表日順のソート

--- logics/api/fetchAllEntryData.ts
+++ logics/api/fetchAllEntryData.ts
@@ -6,24 +6,21 @@ import { EntryType } from "../../types/EntryType";
  * 記事データをすべて取得します
  */
 export const fetchAllEntryData = async (): Promise<EntryType[]> => {
-  const allPortfolioData = await fetchDataFromAPI<PortfolioModel>("portfolio");
+  const allPortfolioData = await fetchDataFromAPI<PortfolioModel>("portfolio", {
+    // 発表日の新しい順にソートする
+    order: "-fields.published_date",
+  });
 
-  return allPortfolioData.items
-    .map((entry) => {
-      const tags: EntryType["tags"] = entry.fields.tags.map(
-        (tagEntry) => tagEntry.fields
-      );
-      return {
-        id: entry.sys.id,
-        ...entry.fields,
-        medium: entry.fields.medium.fields,
-        slide: entry.fields.slide?.fields ?? null,
-        tags,
-      };
-    })
-    .sort(
-      (a, b) =>
-        new Date(b.published_date).getTime() -
-        new Date(a.published_date).getTime()
+  return allPortfolioData.items.map((entry) => {
+    const tags: EntryType["tags"] = entry.fields.tags.map(
+      (tagEntry) => tagEntry.fields
     );
+    return {
+      id: entry.sys.id,
+      ...entry.fields,
+      medium: entry.fields.medium.fields,
+      slide: entry.fields.slide?.fields ?? null,
+      tags,
+    };
+  });
 };
