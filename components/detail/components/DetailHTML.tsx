824411d04ddf26e18efedcaac50d6eae4e6e4190
takeshi.kano <t.kano.624@gmail.com>
2020-09-06 19:50:46 +0900

    ✨記事詳細：画像回りのマージン

--- components/detail/components/DetailHTML.tsx
+++ components/detail/components/DetailHTML.tsx
@@ -1,14 +1,49 @@
 import React from "react";
-import { createDetailHTML } from "./logics/createDetailHTML";
+import {
+  documentToHtmlString,
+  Options,
+} from "@contentful/rich-text-html-renderer";
+import { BLOCKS, INLINES } from "@contentful/rich-text-types";
 import styles from "./DetailHTML.module.scss";
 
-type Props = { detailDocument: Document };
+/**
+ * 記事詳細用HTMLを作成します
+ */
+const createDetailHTML = (detailDocument: Document): string => {
+  const renderingHTMLOption: Partial<Options> = {
+    renderNode: {
+      [INLINES.HYPERLINK]: (node, next) => {
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+        return `<a href="${node.data.uri}"${
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
+          node.data.uri.startsWith("https://mydomain.com")
+            ? ""
+            : ' target="_blank"'
+        }>${next(node.content)}</a>`;
+      },
+      [BLOCKS.EMBEDDED_ASSET]: ({
+        data: {
+          target: { fields },
+        },
+      }) =>
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/restrict-template-expressions
+        `<img loading="lazy" src="${fields.file.url}" width="${fields.file.details.image.width}" height="${fields.file.details.image.height}" alt="${fields.title}"/>`,
+    },
+  };
 
+  const detailHTML = documentToHtmlString(
+    detailDocument as any,
+    renderingHTMLOption
+  );
+  return detailHTML;
+};
 /**
  * 記事詳細用HTML用コンポーネント
  * @param detailDocument
  */
-export const DetailHTML: React.FC<Props> = ({ detailDocument }) => {
+export const DetailHTML: React.FC<{ detailDocument: Document }> = ({
+  detailDocument,
+}) => {
   const detailHTML = createDetailHTML(detailDocument);
 
   return (
