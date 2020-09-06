94e45d377456559bc1441ea7a7ca892357c9086f
takeshi.kano <t.kano.624@gmail.com>
2020-09-07 00:37:48 +0900

    ✨pwa：pwa対応

--- components/base/BasePage.tsx
+++ components/base/BasePage.tsx
@@ -43,14 +43,32 @@ const BasePage: React.FC<Props> = ({
         .join(" ")}
     >
       <Head>
+        <meta charSet="utf-8" />
         <title>{title}</title>
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <meta name="description" content={description} />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content={`@${TwitterId}`} />
         <meta name="og:title" content={title} />
         <meta name="og:image" content={ogImageUrl} />
         <meta name="og:description" content={description} />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          href="/favicon-16x16.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="/favicon-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link rel="apple-touch-icon" href="/apple-icon.png" />
+        <meta name="theme-color" content="#3f3f9d" />
       </Head>
       <div className={styles.wrapper}>
         <AppHeader className={styles.appHeader} />
