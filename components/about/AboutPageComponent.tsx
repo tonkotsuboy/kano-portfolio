8dcba68103e49a77438678bc4759713e70f8f539
takeshi.kano <10419447+tonkotsuboy@users.noreply.github.com>
2021-09-21 20:41:03 +0900

    ✨feat: update profile

--- components/about/AboutPageComponent.tsx
+++ components/about/AboutPageComponent.tsx
@@ -3,36 +3,40 @@ import ExternalLink from "../common/ExternalLink";
 import styles from "./AboutPageComponent.module.scss";
 
 const AboutPageComponent: VFC = () => (
-    <div className={styles.about}>
-      <h1>自己紹介</h1>
-      <img src="/ogimage.png" width="1200" height="630" alt="鹿野 壮" />
-      <p>鹿野 壮（かの たけし）といいます。</p>
-      <p>
-        九州大学芸術工学部音響設計学科を卒業後、Money
-        Forwardでフロントエンドの仕事をしています。とくにTypeScript・JavaScriptが好きで、暇があればコードを書いています。勉強会・技術SNS・ICS
-        MEDIA・Twitterなどで積極的に技術情報を発信中。
-      </p>
-      <p>
-        CSS Nite 2017〜2019ベストセッション受賞。TechFeed Pro公認エキスパート。
-      </p>
-      <h2>書籍</h2>
-      <ul>
-        <li>
-          <ExternalLink
-            url="https://shop.nikkeibp.co.jp/front/commodity/0000/SW1256/"
+  <div className={styles.about}>
+    <h1>自己紹介</h1>
+    <img src="/ogimage.png" width="1200" height="630" alt="鹿野 壮" />
+    <p>鹿野 壮（かの たけし）といいます。</p>
+    <p>
+      九州大学芸術工学部音響設計学科を卒業後、Money
+      Forwardでフロントエンドの仕事をしています。とくにTypeScript・JavaScriptが好きで、暇があればコードを書いています。勉強会・技術SNS・ICS
+      MEDIA・Twitterなどで積極的に技術情報を発信中。
+    </p>
+    <p>
+      CSS Nite 2017〜2019ベストセッション受賞。{" "}
+      <ExternalLink
+        url="https://techfeed.io/people/@tonkotsuboy_com"
+        linkText="TechFeed Pro公認エキスパート"
+      />
+    </p>
+    <h2>書籍</h2>
+    <ul>
+      <li>
+        <ExternalLink
+          url="https://shop.nikkeibp.co.jp/front/commodity/0000/SW1256/"
           linkText="日経ソフトウェア2021年9月号「最新CSS」"
-          />
-        </li>
-        <li>
-          <ExternalLink
-            url="https://shop.nikkeibp.co.jp/front/commodity/0000/SW1250/"
-            linkText="日経ソフトウェア2020年9月号「JavaScript最新仕様 -ECMAScript2020-」"
-          />
-        </li>
-        <li>
-          <ExternalLink
-            url="https://ics.media/entry/19765/"
-            linkText="JavaScriptコードレシピ集"
+        />
+      </li>
+      <li>
+        <ExternalLink
+          url="https://shop.nikkeibp.co.jp/front/commodity/0000/SW1250/"
+          linkText="日経ソフトウェア2020年9月号「JavaScript最新仕様 -ECMAScript2020-」"
+        />
+      </li>
+      <li>
+        <ExternalLink
+          url="https://ics.media/entry/19765/"
+          linkText="JavaScriptコードレシピ集"
         />
       </li>
     </ul>
@@ -44,19 +48,23 @@ const AboutPageComponent: VFC = () => (
           linkText="Twitter"
         />
       </li>
-      <li>
-        <ExternalLink
-          url="https://qiita.com/tonkotsuboy_com"
-          linkText="Qiita（13,500Contributionsで全体約40位）"
-        />
-      </li>
+
       <li>
         <ExternalLink url="https://github.com/tonkotsuboy" linkText="GitHub" />
       </li>
       <li>
         <ExternalLink
-          url="https://github.com/ics-kano"
-          linkText="GitHub（社用）"
+          url="https://techfeed.io/people/@tonkotsuboy_com"
+          linkText="TechFeed Pro"
+        />
+      </li>
+      <li>
+        <ExternalLink url="https://zenn.dev/tonkotsuboy_com" linkText="Zenn" />
+      </li>
+      <li>
+        <ExternalLink
+          url="https://qiita.com/tonkotsuboy_com"
+          linkText="Qiita（13,500Contributionsで全体約40位）"
         />
       </li>
     </ul>
