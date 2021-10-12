0eaf102975219b7fa3b02b290d3a31b936ebb5d0
takeshi.kano <10419447+tonkotsuboy@users.noreply.github.com>
2021-10-12 23:41:02 +0900

    ✨feat: add gtm

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import { VFC } from "react";
+import { useEffect, VFC } from "react";
 import { Provider } from "react-redux";
 import { AppProps } from "next/app";
+import TagManager from "react-gtm-module";
 import { useStore } from "../store";
 
 import "../styles/reset.scss";
@@ -9,6 +10,14 @@ import "../styles/base.scss";
 const AppComponent: VFC<AppProps> = ({ Component, pageProps }) => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
   const store = useStore(pageProps.initialReduxState);
+
+  useEffect(() => {
+    const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
+    if (gtmId == null) {
+      throw new Error("no gtm");
+    }
+    TagManager.initialize({ gtmId });
+  }, []);
 
   return (
     <Provider store={store}>
