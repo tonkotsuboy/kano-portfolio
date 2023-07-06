4435ee4718d3f0f3a354c280e92fb127cc981a35
takeshi.kano <t.kano.624@gmail.com>
2023-07-06 22:52:29 +0900

    Refactor Link components for better readability
    In this commit, I've refactored the use of Link components in several files: `MediumTagList.tsx`, `Author.tsx`, and `EntryList.tsx`. The properties of the Link components were updated to enhance the readability and maintainability of the code. Previously, class names and other attributes were nested under a nested <a> tag within the Link component. Now, these properties are directly applied to the Link components. This eliminates unnecessary nesting and makes the intention of the code more apparent.

--- components/entry/EntryList.tsx
+++ components/entry/EntryList.tsx
@@ -19,10 +19,12 @@ export const EntryList: VFC<Props> = ({ listTitle }) => {
     <div className={styles.entryList}>
       {listTitle && <h1 className={styles.listTitle}>{listTitle}</h1>}
       {entryDataList.map((entryData) => (
-        <Link key={entryData.id} href={`/entry/${entryData.slug}`}>
-          <a aria-label={entryData.title}>
-            <EntryArticle entryData={entryData} isLinkEntry />
-          </a>
+        <Link
+          key={entryData.id}
+          href={`/entry/${entryData.slug}`}
+          aria-label={entryData.title}
+        >
+          <EntryArticle entryData={entryData} isLinkEntry />
         </Link>
       ))}
     </div>
