f9a06dbfaed7f62fc72cd34e03830efe2640be0c
takeshi.kano <t.kano.624@gmail.com>
2023-07-09 17:34:21 +0900

    Recreate medium page and add stylesheet
    The pages/medium/[id].tsx file has been deleted and replaced with app/medium/[slug]/page.tsx to improve code readability and organization. This simplifies logic by eliminating unnecessary imports and providing a more focused approach to fetching and displaying entry data relevant to the specific medium. Moreover, a new stylesheet has been added in app/medium/[slug]/page.css.ts to provide styling for the medium entry page. These changes aim to improve the structure, maintainability, and visual consistency of the codebase.

---
+++ app/medium/[slug]/page.tsx
@@ -0,0 +1,41 @@
+import type { NextPage } from "next";
+
+import { EntryList } from "../../../components/entry/EntryList";
+import { fetchAllEntryData } from "../../../logics/api/fetchAllEntryData";
+import { container } from "./page.css";
+import { Copyright } from "../../components/concerns/Copyright";
+import { fetchMedia } from "../../../logics/api/fetchMedia";
+
+export const generateStaticParams = async (): Promise<string[]> => {
+  const mediumData = await fetchMedia();
+  return mediumData.map((tag) => {
+    return `/medium/${tag.slug}`;
+  });
+};
+
+const getEntryData = async (params: { slug: string }) => {
+  const entryDataList = (await fetchAllEntryData()).filter(
+    (entryData) =>
+      // paramのmediaが含まれているかどうか？
+      entryData.medium?.fields.slug === params.slug,
+  );
+
+  return {
+    entryDataList,
+  };
+};
+
+type Params = { params: { slug: string } };
+
+const Page: NextPage<Params> = async ({ params }) => {
+  const { entryDataList } = await getEntryData(params);
+
+  return (
+    <div className={container}>
+      <EntryList entryDataList={entryDataList} />
+      <Copyright />
+    </div>
+  );
+};
+
+export default Page;
