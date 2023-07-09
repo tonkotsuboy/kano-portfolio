9634db8e74169a183b7986acac405eade15f4d5a
takeshi.kano <t.kano.624@gmail.com>
2023-07-09 21:52:50 +0900

    Recreate DetailHTML component and associated styles
    Moved DetailHTML.tsx and DetailHTML.module.scss from components/detail/components to app/components/concerns/DetailHTML. The move is to streamline the project structure and improve maintainability. Also, 'next/link' and 'next/image' are now being used in DetailHTML.tsx for navigation and image rendering respectively for better performance and efficiency.

--- app/components/concerns/DetailHTML/DetailHTML.tsx
+++ app/components/concerns/DetailHTML/DetailHTML.tsx
@@ -1,18 +1,20 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call, react/jsx-no-target-blank, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/restrict-template-expressions,@typescript-eslint/no-explicit-any */
-import { BLOCKS, Document, INLINES } from "@contentful/rich-text-types";
-import { FC } from "react";
-import {
-  documentToReactComponents,
-  Options,
-} from "@contentful/rich-text-react-renderer";
+import type { Document } from "@contentful/rich-text-types";
+import { BLOCKS, INLINES } from "@contentful/rich-text-types";
+import type { FC } from "react";
+import type { Options } from "@contentful/rich-text-react-renderer";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import styles from "./DetailHTML.module.scss";
-import ExternalLink from "../../common/ExternalLink";
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+import { createHttpsImage } from "../../../utils";
 
 const renderOptions: Options = {
   renderNode: {
     [INLINES.HYPERLINK]: ({ content, data }) => {
       const linkText = (content[0] as any).value as string;
-      return <ExternalLink url={data.uri} linkText={linkText} />;
+      return <Link href={data["uri"]}>{linkText}</Link>;
     },
     [BLOCKS.EMBEDDED_ASSET]: ({
       data: {
@@ -21,18 +23,12 @@ const renderOptions: Options = {
         },
       },
     }) => (
-      // TODO: Imageコンポーネントを使いたい
-      <picture>
-        <source srcSet={`${file.url}?fm=webp`} type="image/webp" />
-        <source srcSet={`${file.url}?fm=jpg&q=50`} type="image/jpeg" />
-        <img
-          loading="lazy"
-          src={file.url}
-          alt={title}
-          width={file.details.image.width}
-          height={file.details.image.height}
-        />
-      </picture>
+      <Image
+        src={createHttpsImage(file.url as string)}
+        width={file.details.image.width}
+        height={file.details.image.height}
+        alt={title}
+      />
     ),
   },
 };
@@ -43,7 +39,7 @@ const renderOptions: Options = {
 export const DetailHTML: FC<{ detailDocument: Document }> = ({
   detailDocument,
 }) => (
-  <div className={styles.detailHTML}>
+  <div className={styles["detailHTML"]}>
     {documentToReactComponents(detailDocument, renderOptions)}
   </div>
 );
