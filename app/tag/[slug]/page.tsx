b4ca0c2bc67fcdd44b3c5859c075dc9eb484c3d6
takeshi.kano <t.kano.624@gmail.com>
2023-07-09 17:50:30 +0900

    Recreate tag page and improve code organization
    The existing framework for the tag page in the file "pages/tag/[id].tsx" was removed and restructured for better code organization and efficiency. The code was redistributed to two new files "app/tag/[slug]/page.css.ts" and "app/tag/[slug]/page.tsx" to clearly separate styling and logic. This would greatly improve readability and maintainability. The new approach also takes advantage of the Next.js's route-based file organization scheme, aligning more with its intended usage style.

---
+++ app/tag/[slug]/page.tsx
@@ -0,0 +1,42 @@
+import type { NextPage } from "next";
+
+import { EntryList } from "../../../components/entry/EntryList";
+import { fetchAllEntryData } from "../../../logics/api/fetchAllEntryData";
+import { container } from "./page.css";
+import { Copyright } from "../../components/concerns/Copyright";
+import { fetchTagList } from "../../../logics/api/fetchTagList";
+
+export const generateStaticParams = async (): Promise<string[]> => {
+  const tagData = await fetchTagList();
+  return tagData.map((tag) => {
+    return `/tag/${tag.slug}`;
+  });
+};
+
+const getEntryData = async (params: { slug: string }) => {
+  const entryDataList = (await fetchAllEntryData()).filter((entryData) => {
+    // タグ内に、paramのタグが含まれているかどうか？
+    return entryData.tags.some(
+      (tagData) => tagData?.fields.slug === params.slug,
+    );
+  });
+
+  return {
+    entryDataList,
+  };
+};
+
+type Params = { params: { slug: string } };
+
+const Page: NextPage<Params> = async ({ params }) => {
+  const { entryDataList } = await getEntryData(params);
+
+  return (
+    <div className={container}>
+      <EntryList entryDataList={entryDataList} />
+      <Copyright />
+    </div>
+  );
+};
+
+export default Page;
