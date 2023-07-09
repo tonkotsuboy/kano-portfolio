91fa69e7829e5bfe73dd102176ce02f3ad4d814c
takeshi.kano <t.kano.624@gmail.com>
2023-07-09 21:54:19 +0900

    Add remote image sources to next.config.js
    In this update, `images` configuration has been added in the `next.config.js` file to allow the use of remote image sources. This change is crucial to allow the application to fetch images from various external servers' defined protocols, hostnames, and ports, thereby improving the flexibility and extendability of content fetching scenarios in the application.

--- next.config.js
+++ next.config.js
@@ -4,6 +4,65 @@ const withVanillaExtract = createVanillaExtractPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.ctfassets.net",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "doorkeeper.jp",
+        port: "",
+      },
+      {
+        protocol: "http",
+        hostname: "cssnite-kobe.jp",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "s3-ap-northeast-1.amazonaws.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "connpass-tokyo.s3.amazonaws.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "files.speakerdeck.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "cssnite-osaka.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "cssnite.jp",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "qiita-user-contents.imgix.net",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "connpass-tokyo.s3.amazonaws.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+      },
+    ],
+  },
 };
 
 module.exports = withVanillaExtract(nextConfig);
