1f72536bfc59b95d106b01da821a81dc71ee9683
takeshi.kano <t.kano.624@gmail.com>
2023-07-09 21:48:54 +0900

    Refactor unit test for better async handling and type safety
    In this commit, the unit test for fetchOgInfo has been refactored. This was done to handle asynchronous operations in a cleaner way and for better type safety. Previously, the returned Promise was forced to expect a string, which could potentially lead to type errors. Now, it accepts any type and explicitly casts to string when necessary. Also, the previous callback fashion done() function is not used anymore in the test, since async/await handles it more straightforwardly.

--- __tests__/unit/logics/fetchOgInfo.sepc.ts
+++ __tests__/unit/logics/fetchOgInfo.sepc.ts
@@ -3,18 +3,20 @@ import { creteHTMLDocument } from "../../../logics/scraping/creteHTMLDocument";
 import { parseMetaInfo } from "../../../logics/scraping/parseMetaInfo";
 
 describe("parseMetaInfo", () => {
-  test("meta情報回りの正常取得", async (done) => {
-    const htmlText = await new Promise<string>((resolve) => {
+  test("meta情報回りの正常取得", async () => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const htmlText = await new Promise<any>((resolve) => {
       request.get("https://www.kyushu-u.ac.jp/ja/", (_err, _response, body) => {
         resolve(body);
       });
     });
 
-    const htmlDocument = creteHTMLDocument(htmlText);
-    const { ogImage, ogTitle, ogDescription } = parseMetaInfo(htmlDocument)!;
+    const htmlDocument = creteHTMLDocument(htmlText as string);
+
+    const { ogImage, ogTitle, ogDescription } =
+      parseMetaInfo(htmlDocument) ?? {};
     expect(ogTitle?.includes("九州大学")).toBeTruthy();
     expect(ogImage?.includes(".png")).toBeTruthy();
     expect(ogDescription?.includes("九州大学")).toBeTruthy();
-    done();
   });
 });
