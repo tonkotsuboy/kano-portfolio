52dde83b20b43c974c76d3530c86dc5f200281a2
takeshi.kano <t.kano.624@gmail.com>
2023-07-09 21:41:39 +0900

    Update layout css to use minHeight instead of height
    Modified the layout.css.ts file to change the height attribute to minHeight. This was done to ensure full viewport height coverage for all elements on the page, regardless of content size.

--- app/layout.css.ts
+++ app/layout.css.ts
@@ -4,7 +4,7 @@ export const wrapper = style({
   display: "grid",
   gridTemplate: `"navigation main" 1fr/240px 1fr`,
   maxWidth: "1080px",
-  height: "100%",
+  minHeight: "100dvh",
   marginInline: "auto",
 });
 
