07936c5bd9e4253f51316aaff6139aab7fa0b79f
takeshi.kano <t.kano.624@gmail.com>
2023-07-09 13:43:10 +0900

    Add vanilla-extract

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
-module.exports = {
+const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
+const withVanillaExtract = createVanillaExtractPlugin();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   swcMinify: true,
 };
+
+module.exports = withVanillaExtract(nextConfig);
