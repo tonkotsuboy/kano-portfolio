d5f6971622025ab4f8c29e08af116ffc77eb484b
takeshi.kano <t.kano.624@gmail.com>
2023-07-10 09:29:22 +0900

    Update test in fetchOgInfo.spec.ts to use hardcoded HTML
    Updated fetchOgInfo.spec.ts unit test script to use hardcoded sample HTML instead of making a request to a live website. The reason for this change was to eliminate the potential for tests to fail due to inconsistencies or changes on the live website. We now test the function "parseMetaInfo" with our controlled HTML string, ensuring our test only verifies the correctness of our own code.

--- app/__tests__/unit/logics/fetchOgInfo.sepc.ts
+++ app/__tests__/unit/logics/fetchOgInfo.sepc.ts
@@ -1,21 +1,36 @@
-import request from "request";
 import { creteHTMLDocument } from "../../../logics/scraping/creteHTMLDocument";
 import { parseMetaInfo } from "../../../logics/scraping/parseMetaInfo";
 
+const sampleHTML = `
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>とんこつ大学</title>
+    <meta name="Keywords" content="とんこつ大学">
+    <meta name="Description" content="とんこつ大学の公式ウェブサイトです">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta property="og:title" content="とんこつ大学">
+    <meta property="og:url" content="https://example.com/ja/">
+    <meta property="og:description" content="とんこつ大学の公式ウェブサイトです">
+    <meta property="og:image" content="https://example.com/thumb.png">
+    <meta property="og:site_name" content="とんこつ大学">
+    <meta property="og:type" content="website" />
+    <meta property="og:locale" content="ja_JP" />
+    </script>
+</head>
+<body>
+</body>>
+</html>
+`;
+
 describe("parseMetaInfo", () => {
   test("meta情報回りの正常取得", async () => {
-    const htmlText = await new Promise((resolve) => {
-      request.get("https://www.kyushu-u.ac.jp/ja/", (_err, _response, body) => {
-        resolve(body);
-      });
-    });
-
-    const htmlDocument = creteHTMLDocument(htmlText as string);
-
+    const htmlDocument = creteHTMLDocument(sampleHTML);
     const { ogImage, ogTitle, ogDescription } =
       parseMetaInfo(htmlDocument) ?? {};
-    expect(ogTitle?.includes("九州大学")).toBeTruthy();
-    expect(ogImage?.includes(".png")).toBeTruthy();
-    expect(ogDescription?.includes("九州大学")).toBeTruthy();
+    expect(ogTitle?.includes("とんこつ大学")).toBe(true);
+    expect(ogImage?.includes("https://example.com/thumb.png")).toBe(true);
+    expect(ogDescription?.includes("とんこつ大学")).toBe(true);
   });
 });
