817c139b1c5a5d2bf0e6f8b80976ed3e52ee37ca
tonkotsuboy <t.kano.624@gmail.com>
2023-07-15 07:43:08 +0900

    Separate detailed styles for entry page
    This commit adds additional CSS classes and updates the entry page layout. The changes include a more detailed style configuration for elements such as the article, header, medium, tags, title, link, and published date. This has been done to improve the visual appeal and readability of the entry page elements. It also includes the introduction of a new function 'parseDate' to simplify the display date format.

--- app/entry/[slug]/page.css.ts
+++ app/entry/[slug]/page.css.ts
@@ -10,3 +10,63 @@ export const video = style({
   width: "100%",
   aspectRatio: "16/9",
 });
+
+export const article = style({
+  display: "grid",
+  gap: vars.spacing["8px"],
+  padding: vars.spacing["16px"],
+  overflow: "hidden",
+  background: "#ffffff",
+  border: "1px solid #f1f3fa",
+  borderRadius: "14px",
+  boxShadow: "2px 8px 30px rgba(48, 55, 120, 0.04)",
+});
+
+export const header = style({
+  display: "flex",
+  alignItems: "center",
+});
+
+export const medium = style({
+  paddingRight: "16px",
+  paddingLeft: "16px",
+  fontSize: "12px",
+  lineHeight: "20px",
+  color: vars.color.primary,
+  background: "#edeef8",
+  border: `1px solid ${vars.color.primary}`,
+  borderRadius: "4px",
+});
+
+export const tagList = style({
+  display: "grid",
+  gridAutoFlow: "column",
+  gap: "6px",
+  height: "100%",
+  marginLeft: "12px",
+});
+
+export const tag = style({
+  fontSize: vars.font.size.xs,
+  color: vars.color.secondary,
+});
+
+export const title = style({
+  fontSize: vars.font.size.l,
+  fontStyle: "normal",
+  fontWeight: "bold",
+  lineHeight: "150%",
+  color: vars.color.text,
+});
+
+export const link = style({
+  fontSize: vars.font.size.xs,
+  fontWeight: "500",
+  color: "var(-primaryColor)",
+});
+
+export const publishedDate = style({
+  display: "block",
+  fontSize: vars.font.size.xs,
+  color: vars.color.text,
+});
