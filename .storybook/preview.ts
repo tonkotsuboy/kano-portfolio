b9ac4260ba50c4e16f0e753b8111fdd9b837da3d
tonkotsuboy <t.kano.624@gmail.com>
2023-07-19 09:59:53 +0900

    Add global styles to Storybook previews
    Added 'reset.css' and 'base.css' to Storybook's preview configuration. This ensures that components will render in a consistent manner across different stories, accurately reflecting design system's styling standards.

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,8 @@
 import type { Preview } from "@storybook/react";
 
+import "../app/styles/reset.css";
+import "../app/styles/base.css";
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
