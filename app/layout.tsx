9a68daa123dab4831a3abe641b48c8642a2bc0a6
Takeshi Kano <t.kano.624@gmail.com>
2026-06-03 15:04:17 +0900

    refactor: Person.description を人物中心の説明に変更
    サイト説明である basicDescription は WebSite.description のみで使い、
    Person.description には about ページの自己紹介と一致する人物中心の文を指定する
    （捏造なし・肩書きは jobTitle と同じ Staff Product Engineer に統一）。

--- app/layout.tsx
+++ app/layout.tsx
@@ -98,7 +98,9 @@ const structuredData = {
       "@id": `${SiteUrl}/#person`,
       "@type": "Person",
       alternateName: ["鹿野 壮", "かの たけし", "tonkotsuboy", "tonkotsuboy_com"],
-      description: basicDescription,
+      // Person は「人物」の説明。サイト説明の basicDescription（WebSite 側で使用）とは分け、
+      // about ページの自己紹介と一致させる（捧造なし）。
+      description: "Ubie株式会社の Staff Product Engineer。TypeScript・CSS を軸にプロダクト開発・執筆・登壇を行う。",
       familyName: "鹿野",
       givenName: "壮",
       image: {
